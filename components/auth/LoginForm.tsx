0fd1710a562d6a7ed57d235dc44f5668d6b1a391
Duc Minh <SteamGTAEVO@gmail.com>
2026-01-22 17:54:42 +0700

    add login form UI with Ant Design

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,8 +1,23 @@
 'use client';
 import { Form, Input, Button } from 'antd';
+// Import các component UI từ Ant Design {Form: khung form tổng ,Input: ô nhập text ,Button: nút bấm}
 
 
-export default function LoginForm() {
-  return <div>Login Form</div>;
+export default function LoginForm() {  // Mới làm UI form đăng nhập
+  return (
+  <Form layout="vertical">
+    <Form.Item label="Username or Email">
+      <Input placeholder="Enter your username or email" />
+    </Form.Item>
+
+    <Form.Item label="Password">
+      <Input.Password placeholder="Enter your password" />
+    </Form.Item>
+
+    <Button type="primary" block>
+      Login
+    </Button>
+  </Form>
+);
 }
     
