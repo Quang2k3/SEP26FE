e46d7230c438c8c8ff9d1a4ce2fc95b3795f722c
Duc Minh <SteamGTAEVO@gmail.com>
2026-02-17 16:31:44 +0700

    chore: new app router structure + login page

--- src/app/verify-email/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function VerifyEmailPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl">Verify Email Page (Coming Soon)</h1>
-    </div>
-  );
-}
